bbfb7e0cecc07d35e6e6f8d58632d8b14ab349aa
zodern <zodern@users.noreply.github.com>
2023-07-19 14:14:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('.swc/node_modules/@swc/core/index.js');
---
+++ install.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+let folderPath = path.resolve(__dirname, './.swc');
+const packageJsonPath = path.resolve(folderPath, 'package.json');
+
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
+}
+if (!fs.existsSync(packageJsonPath)) {
+  fs.writeFileSync(packageJsonPath, JSON.stringify({
+    name: 'swc',
+    version: "1.0.0",
+    dependencies: {
+      "@swc/core": "1.3.70"
+    }
+  }));
+}
+
+console.log('installing @swc/core for this platform and arch');
+
+require('child_process').spawnSync(process.env.NODE, [
+  process.env.npm_execpath,
+  'install'
+], {
+  cwd: folderPath,
+  stdio: 'inherit'
+}).toString().trim();
